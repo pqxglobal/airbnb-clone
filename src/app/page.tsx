ffe0294471ddc6e059d69f363fc3afdc37a2d8c6
pqx <l.r.slegt@gmail.com>
2023-03-07 15:22:35 +0100

    ✨ Banner added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from '../../components/Header'
+import Banner from '../../components/Banner'
 
 export default function Home() {
   return (
     <div className=''>
 
       <Header/>
-      {/* {banner} */}
+      <Banner/>
     </div>
 
   )
